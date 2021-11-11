0a1028af746e5c10286ee2dc6df18bff0c9e9b23
Suwigya Rathore <suwigya@kramphub.com>
2021-11-11 22:07:38 +0100

    add svg

---
+++ scatter-line/main.js
@@ -0,0 +1,12 @@
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+var svg = d3
+  .select("#scatter-line")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
