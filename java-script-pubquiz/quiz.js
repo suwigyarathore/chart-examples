9bfac0b019e86b55a837383796187ae67954e682
Suwigya Rathore <suwigya@kramphub.com>
2021-11-12 00:43:03 +0100

    pub quiz questions

---
+++ java-script-pubquiz/quiz.js
@@ -0,0 +1,53 @@
+//http://www.jsfuck.com/
+
+// *****************************
+// NaN? type
+
+// NaN == NaN
+
+// *****************************
+
+//  '4' - 3
+
+//  '4' + 3
+
+// *****************************
+
+// function first() {
+//   abc = "i am here";
+// }
+
+// function second() {
+//   console.log(abc);
+// }
+
+// ************************************
+// true + true
+
+// ************************************
+// console.log(1);
+// setTimeout(() => console.log(2));
+// console.log(3);
+
+// ************************************
+// Concept of falsy values
+// 1 == true
+
+// ************************************
+// var vs let vs const
+
+// ************************************
+//closures and hoc
+
+// Spread operator
+function abc(a, b, ...rest) {
+  console.log(a, b, rest);
+}
+
+// object spread operator
+const obj = { a: 10, b: 20 };
+
+// spreading arrays
+const arr1 = [1, 2, 4];
+const arr2 = [2, 3, 4];
+//['a',1,2,3,'b',2,3,4]
